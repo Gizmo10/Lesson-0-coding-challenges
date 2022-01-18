129781dd059034205b756c3600618da93e309098
Gizmo10 <sw.ndzala@gmail.com>
2022-01-17 18:59:42 -0900

    The task is complete

--- Task1/src/Task1.java
+++ Task1/src/Task1.java
@@ -1,2 +1,17 @@
 public class Task1 {
+
+    public static void main(String [] args){
+
+        int x = 0;
+        int y = 1;
+
+        System.out.println(x);
+        System.out.println(y);
+
+        x += 3;
+        y += x;
+
+        System.out.println(x);
+        System.out.print(y);
+    }
 }
