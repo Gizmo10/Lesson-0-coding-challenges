7ba3fbbfacc8f5e7e9aabacc3a83ef6360830771
Gizmo10 <sw.ndzala@gmail.com>
2022-01-17 19:31:50 -0900

    Task complete

--- Task3/src/Task3.java
+++ Task3/src/Task3.java
@@ -1,2 +1,12 @@
 public class Task3 {
+
+    public static void main(String [] args){
+
+        hello("Tshepo");
+    }
+
+    public static void hello(String name){
+
+        System.out.println("Hello " + name + "!");
+    }
 }
