2e81b92798ee25d570e0a9283a2026f98d408fae
Gizmo10 <sw.ndzala@gmail.com>
2022-01-19 09:47:54 -0900

    Task complete

--- Task4/src/Task4.java
+++ Task4/src/Task4.java
@@ -1,2 +1,24 @@
-public class git Task4 {
+public class Task4 {
+
+    public static void main(String [] args){
+
+        //An array with test integers to drive and test the function
+        int [] arr = {1,3,6,0,109,11,-2,10};
+
+        for(int i:arr){
+
+            evenOrOdd(i);
+        }
+    }
+
+    public static void evenOrOdd(int number){
+        //All even numbers are divisible by two, with no remainder
+        if(number % 2 == 0){
+
+            System.out.println("even");
+        }else{
+
+            System.out.println("odd");
+        }
+    }
 }
