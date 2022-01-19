08e65c917c104da3bc2969c7db8e939056548692
Gizmo10 <sw.ndzala@gmail.com>
2022-01-19 11:27:35 -0900

    The first version works
    The version which takes three parameters

---
+++ Task6/src/Task6.java
@@ -0,0 +1,40 @@
+public class Task6 {
+
+    public static void main(String [] args){
+
+        double maximum;
+
+        //Testing the function with various arguments
+        maximum = getMaximum(1,2,3);
+        System.out.println(maximum);
+
+        maximum = getMaximum(-1,0,-3);
+        System.out.println(maximum);
+
+        maximum = getMaximum(8,7,-8);
+        System.out.println(maximum);
+
+        maximum = getMaximum(2.3,-0.5,2.2);
+        System.out.println(maximum);
+    }
+
+    public static double getMaximum(double a,double b,double c){
+
+        double maximum;
+
+        if(a > b){
+
+            maximum = a;
+        }else{
+
+            maximum = b;
+        }
+
+        if(c > maximum){
+
+            maximum = c;
+        }
+
+        return maximum;
+    }
+}
