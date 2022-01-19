d8ab7b835e0557c0412b69dbb25cfe7ca3feb46d
Gizmo10 <sw.ndzala@gmail.com>
2022-01-19 10:42:03 -0900

    Method complete

---
+++ Task5/src/Task5.java
@@ -0,0 +1,11 @@
+public class Task5 {
+
+    public static void main(String [] args){
+
+    }
+
+    public double areaOfATriangle(double side1,double side2,double side3){
+
+        return ((side1 + side2 + side3)/ 2.0);
+    }
+}
