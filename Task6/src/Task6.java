b1cfd6a97fc1b33d127c0b73a6075d13a7f76626
Gizmo10 <sw.ndzala@gmail.com>
2022-01-19 15:36:29 -0900

    Task6 with variable parameters done

--- Task6/src/Task6.java
+++ Task6/src/Task6.java
@@ -1,3 +1,6 @@
+/*Because the range of the input was not specified, I assumed the numbers are
+=> 0
+ */
 public class Task6 {
 
     public static void main(String [] args){
@@ -8,33 +11,46 @@ public class Task6 {
         maximum = getMaximum(1,2,3);
         System.out.println(maximum);
 
-        maximum = getMaximum(-1,0,-3);
+        maximum = getMaximum(1,0,3,3,5,7,4,2);
         System.out.println(maximum);
 
-        maximum = getMaximum(8,7,-8);
+        maximum = getMaximum(8,7);
         System.out.println(maximum);
 
-        maximum = getMaximum(2.3,-0.5,2.2);
+        maximum = getMaximum( );
         System.out.println(maximum);
     }
 
-    public static double getMaximum(double a,double b,double c){
+    public static double getMaximum(double... args){
+        /*Maximum is initialised to zero to allow us to have a return value of zero
+        when there are no arguments passed. This is based on the assumption that
+        values passed are => 0
+         */
+        double maximum = 0;
+        int count = 0;
 
-        double maximum;
+        if(args.length == 0){
 
-        if(a > b){
-
-            maximum = a;
+            return maximum;
         }else{
 
-            maximum = b;
+            for(double number: args){
+                //The first element assigned to maximum
+                if(count == 0){
 
-            if(c > maximum){
+                    maximum = number;
+                    count++;
+                }else{
 
-                maximum = c;
+                    if(number > maximum){
+
+                        maximum = number;
+                    }
+                }
             }
         }
 
         return maximum;
     }
+
 }
