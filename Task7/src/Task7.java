df7e909a1a5f3dd0d83834b4d2f42917fca8a23e
Gizmo10 <sw.ndzala@gmail.com>
2022-01-19 16:18:36 -0900

    The methods are tested and work

--- Task7/src/Task7.java
+++ Task7/src/Task7.java
@@ -3,7 +3,10 @@ public class Task7 {
     public static void main(String [] args){
         double temperature;
 
-        temperature = convertCelsiusToFahrenheit(7);
+        temperature = convertCelsiusToFahrenheit(28);
+        System.out.println(temperature);
+
+        temperature = convertFahrenheitToCelsius(temperature);
         System.out.println(temperature);
     }
 
@@ -11,5 +14,11 @@ public class Task7 {
 
         //The formula is : (temperatureInDegreesCelsius * 9/5) + 32
         return((temperature * 9/5.0) + 32);
+    }
+
+    public static double convertFahrenheitToCelsius(double temperature){
+
+        //The formula above was used to derive this
+        return((5*temperature - 160)/9.0);
     }
 }
